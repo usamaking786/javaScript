5ac718971638afe173ef9422d6b1f72d8a2e0861
usamaking786 <usamaahmadansari@gmail.com>
2024-09-23 15:07:24 +0500

    Variables, Ecam Script, and Datatypes

---
+++ index.js
@@ -0,0 +1,39 @@
+// Learning JavaScript here
+
+// varibales
+// let const var
+
+// let is used to declare variable and have a block scope
+// const is used to declare variable and have a block scope and we can not changed it.
+// let name = "usama ahmad";
+// let age = 22;
+// const isMarried = true;
+
+// console.log(name, age, isMarried);
+
+// Data Types
+/*
+
+// 5th Video 
+// Datatypes and ECMA standards |
+
+// String
+let productivity = "high";
+// Number
+let age = 27;
+// we can use numbers till the 2 power of 53 if we used to more data then we can used BigInt for big data
+// Boolean
+let isEngaged = true;
+// null
+const user = null;
+// undefined
+const userData = undefined;
+// BigInt
+const data = BigInt(20);
+// console.log(data)
+// Symbol
+let sym1 = Symbol('some data');
+
+
+// 5th Video is ended here
+*/
